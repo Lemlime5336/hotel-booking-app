7fe3e0d2051412fdc8c3e4634ec00cef6c3fe922
Lemlime5336 <limola5336@gmail.com>
2026-02-13 11:27:56 +0800

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import { useLocation } from 'react-router-dom'
+
+const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner")
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar/>}
+      <div className='min-h-[70vh'>
+      
+      </div>
+    </div>
+  )
+}
+
+export default App
