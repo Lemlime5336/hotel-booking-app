b3430d856226fbfe8dfa8810736f2ede0b5b9682
Lemlime5336 <limola5336@gmail.com>
2026-02-15 12:59:31 +0800

    Create and mount testimonial component

---
+++ client/src/components/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const Testimonial = () => {
+  return (
+    <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 pt-20 pb-30">
+        <Title title="Guest Testimonials" subTitle="Don't believe us? hear it from our customers." />
+    </div>
+  )
+}
+
+export default Testimonial
