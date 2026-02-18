eea11207e0582b8c8fbc7b15d767134097a69e02
Lemlime5336 <limola5336@gmail.com>
2026-02-18 20:56:08 +0800

    Hotel Registration with backend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -105,8 +105,8 @@ const Navbar = () => {
                 ))}
 
                 {user && <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all"
-                onClick={()=> navigate ('/owner')}>
-                    Dashboard
+                onClick={()=> isOwner ? navigate ('/owner') : setShowHotelReg(true)}>
+                    {isOwner ? 'Dashboard' : 'List Your Hotel'}
                 </button>}
 
                 {!user && <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
